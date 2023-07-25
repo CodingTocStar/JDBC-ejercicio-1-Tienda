1e137b48515a515517f02b23c9364bdb97e56a23
gino <nahuel.dmz@outlook.com>
2023-07-25 01:22:05 -0300

    primer repo

---
+++ src/tienda/entidades/Fabricante.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda.entidades;
+
+/**
+ *
+ * @author MiriamNahuel
+ */
+public class Fabricante {
+
+    //ATRIBUTOS
+    private int codigo ;
+    private String nombre;
+    
+    //CONSTRUCTORES
+    public Fabricante(){}
+
+    public Fabricante(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+    
+    //GETTER Y SETTER   
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
---
+++ src/tienda/persistencia/DAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author MiriamNahuel
+ */
+public abstract class DAO {             //es abstracta para no ser instanciada y poder ser heredada
+   
+    //  los atributos para la conexion
+    protected Connection conexion;
+    protected ResultSet resultado;
+    protected Statement sentencia;
+    //----------------------------------------
+    // los datos
+    private final String USER = "root";
+    private final String PASSWORD = "root";
+    private final String DATABASE = "tienda";
+    private final String DRIVER = "com.mysql.jdbc.Driver";
+    //------------------------------------------------------------------
+    
+    // METODOS
+    protected void conectarBase() throws Exception {
+        try {
+            Class.forName(DRIVER);
+            String urlBaseDeDatos = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
+            conexion = DriverManager.getConnection(urlBaseDeDatos, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw ex;
+        }
+    }
+
+}
